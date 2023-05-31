0b4858f11d33b8dda803ac5572ed244a40f68b2d
hoklenghak <lenghakhok.dev@gmail.com>
2023-05-31 19:08:30 +0700

    [comp\mod\icon] ~ added favIcon comp

---
+++ frontend/components/modules/icon/icon.tsx
@@ -0,0 +1,228 @@
+"use client";
+
+import { Fragment } from "react";
+
+const Icon = () => {
+  return (
+    <Fragment>
+      <meta
+        name="color-scheme"
+        content="light"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="57x57"
+        href="/images/light-logo/apple-icon-57x57.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="60x60"
+        href="/images/light-logo/apple-icon-60x60.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="72x72"
+        href="/images/light-logo/apple-icon-72x72.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/images/light-logo/apple-icon-76x76.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/images/light-logo/apple-icon-114x114.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/images/light-logo/apple-icon-120x120.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/images/light-logo/apple-icon-144x144.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/images/light-logo/apple-icon-152x152.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/light-logo/apple-icon-180x180.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/images/light-logo/android-icon-192x192.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/light-logo/favicon-32x32.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/images/light-logo/favicon-96x96.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/light-logo/favicon-16x16.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <link
+        rel="manifest"
+        href="/images/light-logo/manifest.json"
+        media="(prefers-color-scheme: light)"
+      />
+      <meta
+        name="msapplication-TileColor"
+        content="#ffffff"
+        media="(prefers-color-scheme: light)"
+      />
+      <meta
+        name="msapplication-TileImage"
+        content="/images/light-logo/ms-icon-144x144.png"
+        media="(prefers-color-scheme: light)"
+      />
+      <meta
+        name="theme-color"
+        content="#ffffff"
+        media="(prefers-color-scheme: light)"
+      />
+
+      {/*//* Dark Mode Config */}
+      <meta
+        name="color-scheme"
+        content="dark"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="57x57"
+        href="/images/dark-logo/apple-icon-57x57.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="60x60"
+        href="/images/dark-logo/apple-icon-60x60.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="72x72"
+        href="/images/dark-logo/apple-icon-72x72.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="76x76"
+        href="/images/dark-logo/apple-icon-76x76.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="114x114"
+        href="/images/dark-logo/apple-icon-114x114.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="120x120"
+        href="/images/dark-logo/apple-icon-120x120.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/images/dark-logo/apple-icon-144x144.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="152x152"
+        href="/images/dark-logo/apple-icon-152x152.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/dark-logo/apple-icon-180x180.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/images/dark-logo/android-icon-192x192.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/dark-logo/favicon-32x32.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/images/dark-logo/favicon-96x96.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/dark-logo/favicon-16x16.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <link
+        rel="manifest"
+        href="/images/dark-logo/manifest.json"
+        media="(prefers-color-scheme: dark)"
+      />
+      <meta
+        name="msapplication-TileColor"
+        content="#101010"
+        media="(prefers-color-scheme: dark)"
+      />
+      <meta
+        name="msapplication-TileImage"
+        content="/images/dark-logo/ms-icon-144x144.png"
+        media="(prefers-color-scheme: dark)"
+      />
+      <meta
+        name="theme-color"
+        content="#101010"
+        media="(prefers-color-scheme: dark)"
+      />
+    </Fragment>
+  );
+};
+
+export { Icon };
