eece6950106c5e9a8379d3207b226f0a0cd4767d
hoklenghak <lenghakhok.dev@gmail.com>
2023-05-31 16:35:06 +0700

    [comp\ui] ~ added exporter

---
+++ frontend/components/ui/toast/index.tsx
@@ -0,0 +1,10 @@
+export type { ToastProps, ToastActionElement } from "./toast";
+export {
+  ToastProvider,
+  ToastViewport,
+  Toast,
+  ToastTitle,
+  ToastDescription,
+  ToastClose,
+  ToastAction,
+} from "./toast";
