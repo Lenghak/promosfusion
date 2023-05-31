969a6e025650e911229ad0f68a40d7f85f2fc1ff
hoklenghak <lenghakhok.dev@gmail.com>
2023-05-31 19:08:11 +0700

    [comp\mod\html] ~ added darkmode

--- frontend/components/modules/html/html.tsx
+++ frontend/components/modules/html/html.tsx
@@ -2,8 +2,26 @@
 
 import { HtmlHTMLAttributes } from "react";
 
-const Html = ({ children, ...props }: HtmlHTMLAttributes<HTMLHtmlElement>) => {
-  return <html {...props}>{children}</html>;
+import { cn } from "@/lib/utils";
+import { useDarkModeStore } from "@/lib/zustand";
+
+import { useGetStore } from "@/hooks/zustand";
+
+const Html = ({
+  children,
+  className,
+  ...props
+}: HtmlHTMLAttributes<HTMLHtmlElement>) => {
+  const darkMode = useGetStore(useDarkModeStore, (state) => state);
+
+  return (
+    <html
+      {...props}
+      className={cn([className], darkMode?.isDarkMode ? "dark" : "light")}
+    >
+      {children}
+    </html>
+  );
 };
 
 export { Html };
