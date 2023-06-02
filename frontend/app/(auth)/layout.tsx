f6965adff7a5d8ca225cdc3761d566e4a707165c
hoklenghak <lenghakhok.dev@gmail.com>
2023-06-02 13:24:32 +0700

    [app\auth\layout] ~ editing styles on flex

--- frontend/app/(auth)/layout.tsx
+++ frontend/app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ type AuthLayoutProps = {
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
-    <div className="flex h-full w-full place-content-center place-items-center gap-4 p-4 flex-col">
+    <div className="flex h-full w-full items-center justify-center gap-4 p-4 flex-col min-h-screen">
       {/*//* The Auth Side : Extended to half screen after xs-screen | Expand when react xs:screen */}
 
       {/*//* The logo or brand name  */}
