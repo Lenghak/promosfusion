10276d64cd7175f77bc43c8b9ab0ee3f5aadd78a
hoklenghak <lenghakhok.dev@gmail.com>
2023-06-03 21:11:13 +0700

    [app\auth\forgot-password] ~ added back btn

--- frontend/app/(auth)/forgot-password/page.tsx
+++ frontend/app/(auth)/forgot-password/page.tsx
@@ -1,11 +1,23 @@
+import Link from "next/link";
+
 import { FormTitle } from "@/components/modules/auth";
 import { ForgotPasswordForm } from "@/components/modules/auth/forgot-password";
+
+import { ChevronLeft } from "lucide-react";
 
 type ForgotPasswordProps = {};
 
 export default function ForgotPassword({}: ForgotPasswordProps) {
   return (
     <div className="flex h-full w-full max-w-md flex-col items-center gap-8 self-center p-4 md:max-w-none">
+      <Link
+        href={"/sign-in"}
+        className="flex gap-4 items-center justify-center hover:underline hover:underline-offset-2 fixed top-8 left-8"
+      >
+        <ChevronLeft />
+        <span>Back</span>
+      </Link>
+
       <FormTitle
         title={"Reset Password"}
         subTitle={
