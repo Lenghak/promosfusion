a16920249ff32c50c1d7031ce08772af4dcdfbac
hoklenghak <lenghakhok.dev@gmail.com>
2023-06-04 15:27:40 +0700

    [comp\provider\route-pr] ~ moved to provider dir

--- frontend/components/providers/route-progress/route-progress-provider.tsx
+++ frontend/components/providers/route-progress/route-progress-provider.tsx
@@ -4,22 +4,16 @@ import React, { Fragment } from "react";
 
 import ProgressBar from "next-nprogress-bar";
 
-import { useDarkModeStore } from "@/lib/zustand";
-
-import { useGetStore } from "@/hooks/zustand";
-
 type RouteProgressProviderProps = {
   children: React.ReactNode;
 };
 
 const RouteProgressProvider = ({ children }: RouteProgressProviderProps) => {
-  const darkMode = useGetStore(useDarkModeStore, (state) => state);
-
   return (
     <Fragment>
       <ProgressBar
         height="4px"
-        color={darkMode?.isDarkMode ? "#2563eb" : "#3b82f6"}
+        color={true ? "#2563eb" : "#3b82f6"}
         options={{ showSpinner: false }}
         appDirectory
       />
