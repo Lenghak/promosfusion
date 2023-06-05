832c4fc02f58410266c02418baf45882c931204b
hoklenghak <lenghakhok.dev@gmail.com>
2023-06-06 00:07:16 +0700

    [com\mod\topbar] ~ added sidebar toggle

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,13 @@
 import { SideMenu } from "@/components/modules/side-menu";
+import { TopBar } from "@/components/modules/top-bar";
 
 export default function Home() {
   return (
-    <main className="flex items-center">
+    <main className="flex items-start">
       <SideMenu />
+      <section className="flex h-full w-full flex-col px-4">
+        <TopBar />
+      </section>
     </main>
   );
 }
--- frontend/components/modules/top-bar/top-bar.tsx
+++ frontend/components/modules/top-bar/top-bar.tsx
@@ -1,7 +1,32 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+import { useSideMenuStore } from "@/lib/zustand";
+
+import { SidebarClose, SidebarOpen } from "lucide-react";
+
 type TopbarProps = {};
 
 const TopBar = ({}: TopbarProps) => {
-  return <section className="w-full"></section>;
+  const { isSideMenuOpen, setSideMenu } = useSideMenuStore((state) => state);
+
+  return (
+    <section className="relative flex h-[4.5rem] max-h-[4.5rem] w-full items-center border-b border-solid">
+      {/*//* Toggle Button */}
+      <Button
+        variant={"ghost"}
+        className="border border-solid"
+        onClick={() => setSideMenu(!isSideMenuOpen)}
+      >
+        {isSideMenuOpen ? (
+          <SidebarClose size={20} />
+        ) : (
+          <SidebarOpen size={20} />
+        )}
+      </Button>
+    </section>
+  );
 };
 
 export { TopBar };
