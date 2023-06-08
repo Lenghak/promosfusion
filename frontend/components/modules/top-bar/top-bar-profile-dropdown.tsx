4d563b38578cfb75d656b696c141be23e10052b5
Lenghak <lenghakhok.dev@gmail.com>
2023-06-08 21:45:13 +0700

    [com\mod\top-bar] ~ added profile pic

---
+++ frontend/components/modules/top-bar/top-bar-profile-dropdown.tsx
@@ -0,0 +1,81 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import {
+  Laptop2,
+  LogOut,
+  Moon,
+  Palette,
+  Settings,
+  Sun,
+  User,
+} from "lucide-react";
+
+type Props = {};
+
+const TopBarProfileDropdown = ({}: Props) => {
+  return (
+    <DropdownMenu modal>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="cursor-pointer">
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-52">
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <User className="mr-4 h-7 w-[18px]" />
+            <span>Profile</span>
+          </DropdownMenuItem>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <Palette className="mr-4 h-7 w-[18px]" />
+              <span>Theme</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent className="w-48">
+                <DropdownMenuItem>
+                  <Sun className="mr-4 h-7 w-[18px]" />
+                  <span>Light Mode</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Moon className="mr-4 h-7 w-[18px]" />
+                  <span>Dark Mode</span>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                  <Laptop2 className="mr-4 h-7 w-[18px]" />
+                  <span>System</span>
+                </DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+          <DropdownMenuItem>
+            <Settings className="mr-4 h-7 w-[18px]" />
+            <span>Settings</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogOut className="mr-4 h-7 w-[18px]" />
+          <span>Log out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export { TopBarProfileDropdown };
