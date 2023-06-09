44f19c16db6f8fc8576d0b81c545a8f8f7af63ed
Lenghak <lenghakhok.dev@gmail.com>
2023-06-09 14:51:36 +0700

    [com\mod\top-bar] ~ added changable themes

--- frontend/components/modules/top-bar/top-bar-profile-dropdown.tsx
+++ frontend/components/modules/top-bar/top-bar-profile-dropdown.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   DropdownMenu,
@@ -12,6 +16,7 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
+import { useTheme } from "@wits/next-themes";
 import {
   Laptop2,
   LogOut,
@@ -25,17 +30,20 @@ import {
 type Props = {};
 
 const TopBarProfileDropdown = ({}: Props) => {
+  const { setTheme } = useTheme();
+  const router = useRouter();
+
   return (
-    <DropdownMenu modal>
+    <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Avatar className="cursor-pointer">
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-52">
+      <DropdownMenuContent className="w-52 font-medium">
         <DropdownMenuGroup>
-          <DropdownMenuItem>
+          <DropdownMenuItem onClick={() => router.push("/profile")}>
             <User className="mr-4 h-7 w-[18px]" />
             <span>Profile</span>
           </DropdownMenuItem>
@@ -45,23 +53,23 @@ const TopBarProfileDropdown = ({}: Props) => {
               <span>Theme</span>
             </DropdownMenuSubTrigger>
             <DropdownMenuPortal>
-              <DropdownMenuSubContent className="w-48">
-                <DropdownMenuItem>
+              <DropdownMenuSubContent className="w-48 font-medium">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
                   <Sun className="mr-4 h-7 w-[18px]" />
                   <span>Light Mode</span>
                 </DropdownMenuItem>
-                <DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
                   <Moon className="mr-4 h-7 w-[18px]" />
                   <span>Dark Mode</span>
                 </DropdownMenuItem>
-                <DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
                   <Laptop2 className="mr-4 h-7 w-[18px]" />
                   <span>System</span>
                 </DropdownMenuItem>
               </DropdownMenuSubContent>
             </DropdownMenuPortal>
           </DropdownMenuSub>
-          <DropdownMenuItem>
+          <DropdownMenuItem onClick={() => router.push("/settings")}>
             <Settings className="mr-4 h-7 w-[18px]" />
             <span>Settings</span>
           </DropdownMenuItem>
