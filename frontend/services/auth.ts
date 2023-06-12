b1ec0d4b26209abeef0c5df04dcb4ceb0df76219
Lenghak <lenghakhok.dev@gmail.com>
2023-06-12 16:30:31 +0700

    [services] ~ added auth services

---
+++ frontend/services/auth.ts
@@ -0,0 +1,23 @@
+import { signInService, signUpService } from "@/lib/axios/auth";
+
+import {
+  UserRegisterCredentials,
+  UserSignInCredentails,
+} from "@/types/api/auth";
+import { useMutation } from "@tanstack/react-query";
+
+const useSignUpService = (data: UserRegisterCredentials) => {
+  return useMutation({
+    mutationKey: ["sign-up"],
+    mutationFn: async () => await signUpService(data),
+  });
+};
+
+const useSignInService = (data: UserSignInCredentails) => {
+  return useMutation({
+    mutationKey: ["sign-in"],
+    mutationFn: async () => await signInService(data),
+  });
+};
+
+export { useSignUpService, useSignInService };
