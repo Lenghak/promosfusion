714bee71b7524dabc59fa3f393031dd16f6410a8
Lenghak <lenghakhok.dev@gmail.com>
2023-06-13 19:26:11 +0700

    [middleware] ~ prevent from going to login page

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,30 +2,19 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { getToken } from "next-auth/jwt";
 
-const AUTH_PATHS = [
-  /^\/sign-up/,
-  /^\/sign-in/,
-  /^\/forgot-password/,
-  /^\/reset-password/,
-];
-
 // This function can be marked `async` if using `await` inside
 export default async function middleware(request: NextRequest) {
   const token = await getToken({
     req: request,
   });
-  const pathname = request.nextUrl.pathname;
 
-  const isAuthPath = AUTH_PATHS.some((regex) => regex.test(pathname));
-
-  if (token && isAuthPath)
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+  if (token) return NextResponse.redirect(new URL("/dashboard", request.url));
 
   return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  //   matcher: ["/sign-in", "/sign-up", "/forgot-password", "reset-password"],
-  matcher: ["/((?!api|_next|static|favicon.ico|svg).*)"],
+  //   matcher: [],
+  matcher: ["/sign-in", "/sign-up", "/forgot-password", "/reset-password"],
 };
