8b5d400929f2d42a37b669f9470715e455db1985
Lenghak <lenghakhok.dev@gmail.com>
2023-06-13 21:30:57 +0700

    [middleware] ~ added working auth middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,19 +2,30 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { getToken } from "next-auth/jwt";
 
+const AUTH_PATHS = [
+  /^\/sign-in/,
+  /^\/sign-up/,
+  /^\/forgot-password/,
+  /^\/reset-password/,
+];
+
 // This function can be marked `async` if using `await` inside
 export default async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+  const isAuthPath = AUTH_PATHS.some((path) => path.test(pathname));
   const token = await getToken({
     req: request,
   });
 
-  if (token) return NextResponse.redirect(new URL("/dashboard", request.url));
+  if (token && isAuthPath)
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+
+  if (!isAuthPath && !token)
+    return NextResponse.redirect(new URL("/sign-in", request.url));
 
   return NextResponse.next();
 }
 
-// See "Matching Paths" below to learn more
 export const config = {
-  //   matcher: [],
-  matcher: ["/sign-in", "/sign-up", "/forgot-password", "/reset-password"],
+  matcher: ["/((?!api|_next|favicon.ico$|svg|coupon).*)"],
 };
