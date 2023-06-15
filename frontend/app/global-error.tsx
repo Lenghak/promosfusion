2b2c0ddb131b7c7d097e81ab7c9abfaba9a0a2d6
Lenghak <lenghakhok.dev@gmail.com>
2023-06-15 10:59:01 +0700

    [global-error] ~ removed use client

--- frontend/app/global-error.tsx
+++ frontend/app/global-error.tsx
@@ -1,10 +1,8 @@
-"use client";
-
 type ErrorProps = {};
 
 export default function Error({}: ErrorProps) {
   return (
-    <div className="w-full h-full min-h-screen grid place-content-center place-items-center">
+    <div className="grid h-full min-h-screen w-full place-content-center place-items-center">
       <span className="text-xl">
         <b>500 |</b> Internal Server Error
       </span>
