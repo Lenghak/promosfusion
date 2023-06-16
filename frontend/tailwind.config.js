cbfc2e11b377ffd1db3196d6f7205f28ac812bfc
Lenghak <lenghakhok.dev@gmail.com>
2023-06-16 16:09:14 +0700

    [tailwind.config] ~ added up-down animation

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -75,10 +75,15 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: 0 },
         },
+        "up-down": {
+          "0%, 100%": { transform: "translateY(0)" },
+          '50%': { transform: 'translateY(100%)' },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "up-down": "up-down 3s ease-in-out infinite"
       },
     },
   },
