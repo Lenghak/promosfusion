f0cf28ae061c7aaa6cb5857336abf9242b450c14
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-24 22:48:53 +0700

    [services\coupon] ~ added Op Up coupon

--- frontend/services/coupon.ts
+++ frontend/services/coupon.ts
@@ -5,7 +5,7 @@ import {
   useVerifyCoupon,
 } from "@/lib/axios/coupon";
 
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const useProvideCouponService = (campaignId: string) => {
   const provideCoupon = useProvideCoupon(campaignId);
@@ -21,16 +21,41 @@ const useGetCouponService = (couponId: string) => {
     queryKey: ["coupon"],
     queryFn: async () => (await getCoupon(couponId)).data,
     refetchOnMount: true,
-    refetchOnWindowFocus: false,
+    refetchOnReconnect: true,
   });
 };
 
 const useRequestCouponService = (couponId: string) => {
   const requestCoupon = useRequestCoupon(couponId);
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationKey: ["coupon-request"],
     mutationFn: async () => (await requestCoupon()).data,
+    onMutate: async (_newState) => {
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries({ queryKey: ["coupon"] });
+
+      // Snapshot the previous value
+      const previousCoupon = queryClient.getQueryData(["coupon"]);
+
+      // Optimistically update to the new value
+      queryClient.setQueryData(["coupon-request"], (old) => old);
+
+      // Return a context object with the snapshotted value
+      return { previousCoupon };
+    },
+    // If the mutation fails,
+    // use the context returned from onMutate to roll back
+    onError: (_err, _newState, context) => {
+      queryClient.setQueryData(["coupon"], context?.previousCoupon);
+    },
+    // Always refetch after error or success:
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ["coupon"] });
+    },
+    retry: 3,
   });
 };
 
