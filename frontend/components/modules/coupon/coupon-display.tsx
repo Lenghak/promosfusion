b90e2e8fdfcf72dea2ffa6f2647d0797539ddac2
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-24 22:47:28 +0700

    [com\mod\coupon] ~ added claim and verify btn

--- frontend/components/modules/coupon/coupon-display.tsx
+++ frontend/components/modules/coupon/coupon-display.tsx
@@ -1,25 +1,58 @@
 "use client";
 
-import { useGetCouponService } from "@/services/coupon";
+import { useEffect } from "react";
+
+import { usePathname, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
+import {
+  useGetCouponService,
+  useRequestCouponService,
+  useVerifyCouponService,
+} from "@/services/coupon";
 
 import { Loader2 } from "lucide-react";
+import { useSession } from "next-auth/react";
 
 import { CouponContent } from "./coupon-content";
-import CouponError from "./coupon-error";
+import { CouponError } from "./coupon-error";
 import { CouponQR } from "./coupon-qr";
 
-type Props = {
+type CouponDisplayProps = {
   couponId: string;
 };
 
 //! This page is only for the SSR
-const CouponDisplay = ({ couponId }: Props) => {
+const CouponDisplay = ({ couponId }: CouponDisplayProps) => {
+  const pathName = usePathname();
+
+  const { replace } = useRouter();
+
+  const { data: session } = useSession();
+
   const {
-    data: axiosResponse,
-    isError,
-    isLoading,
+    data: coupon,
+    isError: isGetCouponError,
+    isLoading: isGettingCoupon,
+    isFetching: isFetchingCoupon,
   } = useGetCouponService(couponId);
-  return axiosResponse?.cuid && !isError && !isLoading ? (
+
+  const {
+    mutate: requestCoupon,
+    isLoading: isRequesting,
+    isSuccess: isRequested,
+    data: response,
+  } = useRequestCouponService(coupon?.cuid!!);
+
+  useEffect(() => {
+    if (isRequested)
+      replace(`${pathName}/?token=${response.data.token}`, {
+        forceOptimisticNavigation: true,
+      });
+  }, [isRequested, pathName, response, replace]);
+
+  return coupon ? (
     <div
       className={
         "relative flex h-fit w-full max-w-xs flex-col place-self-center"
@@ -27,27 +60,41 @@ const CouponDisplay = ({ couponId }: Props) => {
     >
       {/* Coupon Content */}
       <CouponContent
-        cuid={axiosResponse?.cuid}
-        logo={axiosResponse?.couponDisplay?.logo}
-        companyName={axiosResponse?.couponDisplay?.campany}
-        couponType={axiosResponse?.couponDisplay?.promotion}
-        title={axiosResponse?.couponDisplay?.title}
-        description={axiosResponse?.couponDisplay?.description}
+        cuid={coupon?.cuid}
+        logo={coupon?.couponDisplay?.logo}
+        companyName={coupon?.couponDisplay?.campany}
+        couponType={coupon?.couponDisplay?.promotion}
+        title={coupon?.couponDisplay?.title}
+        description={coupon?.couponDisplay?.description}
       />
 
       <CouponQR
         cuid={couponId}
-        token={couponId} // This is the token
+        token={response?.data.token ?? couponId} // This is the token
+        action={
+          coupon?.currentStatus === "valid" && session ? (
+            <Button>Verify</Button>
+          ) : coupon?.currentStatus === "new" && !session ? (
+            <Button
+              onClick={() => requestCoupon()}
+              disabled={isRequesting}
+            >
+              {!isRequesting ? <Loader2 size={18} /> : "Claim"}
+            </Button>
+          ) : null
+        }
       />
+
+      {/* Action Buttons */}
     </div>
-  ) : isLoading ? (
+  ) : isGettingCoupon || isFetchingCoupon ? (
     <div className="flex h-full w-full items-center justify-center">
       <Loader2
         size={48}
         className="animate-spin"
       />
     </div>
-  ) : isError ? (
+  ) : isGetCouponError ? (
     <CouponError />
   ) : null;
 };
