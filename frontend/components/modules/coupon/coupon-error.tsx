4e17afa99743c7fc8ff28088a3360fc06b68797d
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-24 18:36:41 +0700

    [com\mod\coupon] ~ added error comp

---
+++ frontend/components/modules/coupon/coupon-error.tsx
@@ -0,0 +1,8 @@
+export default function CouponError() {
+  return (
+    <div className="flex h-full w-full items-center justify-center">
+      <span className="font-bold">Error | </span>{" "}
+      <span>There was an error getting your coupon.</span>
+    </div>
+  );
+}
