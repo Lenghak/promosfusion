d445962f2d24a731f75ca9f0dfdd0076a6a1eea7
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-24 22:49:21 +0700

    [com\mod\coupon] ~ adjust component export

--- frontend/components/modules/coupon/coupon-error.tsx
+++ frontend/components/modules/coupon/coupon-error.tsx
@@ -1,10 +1,12 @@
-export default function CouponError() {
+const CouponError = () => {
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-full w-full items-center justify-center text-center">
       <span>
         <span className="font-bold">Error | </span>There was an error getting
         your coupon.
       </span>
     </div>
   );
-}
+};
+
+export { CouponError };
