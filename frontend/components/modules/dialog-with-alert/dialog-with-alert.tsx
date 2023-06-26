3eb57331e3a5d04b7fcde9e684ba6b16da8f45e8
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-26 12:52:03 +0700

    [com\mod\dialog-with-alert] ~ added

---
+++ frontend/components/modules/dialog-with-alert/dialog-with-alert.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { useState } from "react";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+type DialogWithAlertProps = {
+  dialogTrigger: React.ReactNode;
+  dialogTitle?: React.ReactNode;
+  dialogDescription?: React.ReactNode;
+  alertTitle?: React.ReactNode;
+  alertDescription?: React.ReactNode;
+};
+
+const DialogWithAlert = ({
+  dialogTrigger,
+  dialogDescription,
+  dialogTitle,
+  alertTitle,
+  alertDescription,
+}: DialogWithAlertProps) => {
+  const [dialogs, setDialogs] = useState({
+    dialogOpen: false,
+    alertOpen: false,
+    confirmClose: false,
+  });
+
+  return (
+    <>
+      <Dialog
+        onOpenChange={(open) => {
+          open
+            ? setDialogs((prev) => ({ ...prev, dialogOpen: true }))
+            : setDialogs((prev) => ({
+                ...prev,
+                alertOpen: true,
+              }));
+        }}
+        open={dialogs.dialogOpen}
+      >
+        {dialogTrigger}
+        <DialogContent className="h-screen sm:h-fit">
+          <DialogHeader>
+            <DialogTitle>{dialogTitle}</DialogTitle>
+            <DialogDescription>{dialogDescription}</DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
+      <AlertDialog open={dialogs.alertOpen}>
+        <AlertDialogTrigger />
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>{alertTitle}</AlertDialogTitle>
+            <AlertDialogDescription>{alertDescription}</AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel
+              onClick={() =>
+                setDialogs((prev) => ({
+                  ...prev,
+                  dialogOpen: true,
+                  alertOpen: false,
+                }))
+              }
+            >
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction
+              onClick={() =>
+                setDialogs((prev) => ({
+                  ...prev,
+                  dialogOpen: false,
+                  alertOpen: false,
+                }))
+              }
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
+};
+
+export { DialogWithAlert };
