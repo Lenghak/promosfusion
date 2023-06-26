98c224f8b30fb0a14fcc51f8ebd15b042ec5f89f
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-26 19:10:34 +0700

    [types\member] ~ added member types

--- frontend/types/member.ts
+++ frontend/types/member.ts
@@ -1,4 +1,50 @@
+export type Members = {
+  data: Member[];
+  links: MembersLink;
+  meta: Meta;
+};
+
 export type Member = {
+  id: number;
+  uuid: string;
+  name: string;
+  phone: string;
+  email: string;
+  role: string;
+  status: string;
+  type: string;
+  avatar: any;
+  createdAt: string;
+  updatedAt: string;
+};
 
-}
+export type MembersLink = {
+  first: string;
+  last: string;
+  prev: any;
+  next: any;
+};
 
+export type Meta = {
+  current_page: number;
+  from: number;
+  last_page: number;
+  links: MemberMetaLinks[];
+  path: string;
+  per_page: number;
+  to: number;
+  total: number;
+};
+
+export type MemberMetaLinks = {
+  url?: string;
+  label: string;
+  active: boolean;
+};
+
+export type CreateMemberData = {
+  name: string;
+  email: string;
+  role: string;
+  password: string;
+};
