4e559c263840c3ff2e9a3e1ea336f69f9540644d
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-26 13:45:23 +0700

    [com\mod\coupon] ~ use dialog-with-alert

--- frontend/components/modules/coupon/coupon-provide.tsx
+++ frontend/components/modules/coupon/coupon-provide.tsx
@@ -1,32 +1,13 @@
 "use client";
 
-import { useState } from "react";
-
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
+import { DialogTrigger } from "@/components/ui/dialog";
 
 import { useProvideCouponService } from "@/services/coupon";
 
 import { Loader2, Plus } from "lucide-react";
 
+import { DialogWithAlert } from "../dialog-with-alert";
 import { CouponContent } from "./coupon-content";
 import { CouponQR } from "./coupon-qr";
 
@@ -42,138 +23,80 @@ const CouponProvide = ({ campaignId }: CouponProvideProps) => {
     isLoading: isGenerating,
   } = useProvideCouponService(campaignId);
 
-  const [dialogs, setDialogs] = useState({
-    dialogOpen: false,
-    alertOpen: false,
-    confirmClose: false,
-  });
-
   const coupon = generateResponse?.data.data;
 
   return (
-    <>
-      <Dialog
-        onOpenChange={(open) => {
-          open
-            ? setDialogs((prev) => ({ ...prev, dialogOpen: true }))
-            : setDialogs((prev) => ({
-                ...prev,
-                alertOpen: true,
-              }));
-        }}
-        open={dialogs.dialogOpen}
-      >
+    <DialogWithAlert
+      dialogTitle={isGenerateError ? "Generate Error" : "Providing Coupon"}
+      dialogDescription={
+        isGenerateError
+          ? "There was a problem generating the coupon."
+          : "The customer must scan this coupon to claim."
+      }
+      dialogTrigger={
         <DialogTrigger
           className="p-2"
           onClick={() => generateCoupon()}
         >
           <Plus size={18} />
         </DialogTrigger>
-        <DialogContent className="h-screen sm:h-fit">
-          <DialogHeader>
-            <DialogTitle>
-              {isGenerateError ? "Generate Error" : "Providing Coupon"}
-            </DialogTitle>
-            <DialogDescription>
-              {isGenerateError
-                ? "There was a problem generating the coupon."
-                : "The customer must scan this coupon to claim."}
-            </DialogDescription>
-          </DialogHeader>
+      }
+      alertTitle={"Are you absolutely sure?"}
+      alertDescription={
+        "You will leave this page. You can view the coupon later by select from campaign."
+      }
+    >
+      {
+        <div
+          className={
+            "relative flex h-fit w-full max-w-xs flex-col items-center justify-center place-self-center"
+          }
+        >
+          {isGenerating ? (
+            <>
+              <Loader2
+                size={48}
+                className="my-4 animate-spin"
+              />
+              <span>Generating Coupon ...</span>
+            </>
+          ) : coupon ? (
+            <>
+              {/*//* Coupon Content */}
+              <CouponContent
+                cuid={coupon?.cuid}
+                logo={coupon?.couponDisplay.logo}
+                companyName={coupon?.couponDisplay.campany}
+                couponType={coupon?.couponDisplay.promotion}
+                title={coupon?.couponDisplay.title}
+                description={coupon?.couponDisplay.description}
+                status={coupon.currentStatus}
+              />
 
-          <div
-            className={
-              "relative flex h-fit w-full max-w-xs flex-col items-center justify-center place-self-center"
-            }
-          >
-            {isGenerating ? (
-              <>
-                <Loader2
-                  size={48}
-                  className="my-4 animate-spin"
-                />
-                <span>Generating Coupon ...</span>
-              </>
-            ) : coupon ? (
-              <>
-                {/*//* Coupon Content */}
-                <CouponContent
-                  cuid={coupon?.cuid}
-                  logo={coupon?.couponDisplay.logo}
-                  companyName={coupon?.couponDisplay.campany}
-                  couponType={coupon?.couponDisplay.promotion}
-                  title={coupon?.couponDisplay.title}
-                  description={coupon?.couponDisplay.description}
-                  status={coupon.currentStatus}
-                />
-
-                <CouponQR
-                  cuid={coupon.cuid}
-                  token={`${process.env.NEXT_PUBLIC_URL}/coupons/${coupon.cuid}`}
-                />
-              </>
-            ) : isGenerateError ? (
-              <>
-                <div className="flex w-full gap-4">
-                  <Button
-                    variant={"outline"}
-                    className="w-40"
-                    onClick={() =>
-                      setDialogs((prev) => ({ ...prev, dialogOpen: false }))
-                    }
-                  >
-                    Cancel
-                  </Button>
-                  <Button
-                    className="w-40"
-                    onClick={() => generateCoupon()}
-                  >
-                    Rety
-                  </Button>
-                </div>
-              </>
-            ) : null}
-          </div>
-        </DialogContent>
-      </Dialog>
-
-      <AlertDialog open={dialogs.alertOpen}>
-        <AlertDialogTrigger />
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              You will leave this page. You can view the coupon later by select
-              from campaign.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel
-              onClick={() =>
-                setDialogs((prev) => ({
-                  ...prev,
-                  dialogOpen: true,
-                  alertOpen: false,
-                }))
-              }
-            >
-              Cancel
-            </AlertDialogCancel>
-            <AlertDialogAction
-              onClick={() =>
-                setDialogs((prev) => ({
-                  ...prev,
-                  dialogOpen: false,
-                  alertOpen: false,
-                }))
-              }
-            >
-              Continue
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    </>
+              <CouponQR
+                cuid={coupon.cuid}
+                token={`${process.env.NEXT_PUBLIC_URL}/coupons/${coupon.cuid}`}
+              />
+            </>
+          ) : isGenerateError ? (
+            <>
+              <div className="flex w-full flex-col items-center justify-center gap-4">
+                <span className="text-center text-sm">
+                  Looks like we have a problem generating coupon. Would you like
+                  to retry?
+                </span>
+                <Button
+                  className="w-40"
+                  onClick={() => generateCoupon()}
+                >
+                  Rety
+                </Button>
+              </div>
+            </>
+          ) : null}
+        </div>
+      }
+    </DialogWithAlert>
   );
 };
 
