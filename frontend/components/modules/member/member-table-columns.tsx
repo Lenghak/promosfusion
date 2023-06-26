d05b5734d0ffc9bea71f7eef933e1bc83e45e7c5
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-27 00:10:42 +0700

    [com\mod\member-table-content] ~ add columns def

---
+++ frontend/components/modules/member/member-table-columns.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MoreHorizontal } from "lucide-react";
+
+import { AvatarCard } from "../avatar-card";
+
+import { Member } from "@/types/member";
+
+export const columns: ColumnDef<Member>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "uuid",
+    header: "ID",
+  },
+  {
+    accessorKey: "name",
+    header: "User",
+    cell: ({ row }) => {
+      return (
+        <AvatarCard
+          name={row.getValue("name")}
+          image={row.getValue("avatar")}
+          info={row.getValue("email")}
+        />
+      );
+    },
+  },
+  {
+    accessorKey: "email",
+    header: "Email",
+  },
+  {
+    accessorKey: "role",
+    header: "Role",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="ghost"
+              className="h-8 w-8 p-0"
+            >
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem>Assign Shop</DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View Member</DropdownMenuItem>
+            <DropdownMenuItem>Copy Email</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
