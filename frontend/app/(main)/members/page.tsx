4affd1d40638c42e62ace4b617ad599ba45ac1c6
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-27 00:10:16 +0700

    [app\(main)\members] ~ added api call

--- frontend/app/(main)/members/page.tsx
+++ frontend/app/(main)/members/page.tsx
@@ -1,12 +1,16 @@
-// This memebers page fetch the data & show he list of members
+// This members page fetch the data & show he list of members
 import { MemberCreateForm } from "@/components/modules/member/member-create-form";
 import { PageTitle } from "@/components/modules/page-title";
 
-// import { getQueryClient } from "@/lib/react-query";
+import { getMembers } from "@/lib/axios/member";
+import { getQueryClient } from "@/lib/react-query";
 
-export default function Members() {
-  // const queryClient = getQueryClient()
-  // queryClient.prefetchQuery([])
+import { dehydrate, Hydrate } from "@tanstack/react-query";
+
+export default async function Members() {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery(["members"], async () => await getMembers());
+  const dehydratedState = dehydrate(queryClient);
 
   return (
     <div className="flex h-full w-full flex-col">
@@ -14,7 +18,9 @@ export default function Members() {
         title="Dashboard"
         description="View all your visual data associated with coupons"
       />
-      <MemberCreateForm />
+      <Hydrate state={dehydratedState}>
+        <MemberCreateForm />
+      </Hydrate>
     </div>
   );
 }
