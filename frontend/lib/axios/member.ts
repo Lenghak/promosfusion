e4d72d7ed7160f3e0bf6354f0d1f1dc2ccb15895
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-26 19:06:59 +0700

    [lib/axios/coupon] ~ remove uneccessary invalidate

--- frontend/lib/axios/member.ts
+++ frontend/lib/axios/member.ts
@@ -2,17 +2,18 @@ import { useAxiosAuth } from "@/hooks/use-axios-auth";
 
 import { authorizeAxios } from "./authorize";
 
+import { Members } from "@/types/member";
+
 //* get member function for SSR
 const getMembers = async () => {
   const authorizedAxios = await authorizeAxios();
-  return authorizedAxios.get("/users");
+  return authorizedAxios.get<Members>("/users");
 };
 
 //* get memeber function for client side
 const useGetMembers = async () => {
   const authorizedAxios = useAxiosAuth();
-  return () => authorizedAxios.get("/users");
+  return () => authorizedAxios.get<Members>("/users");
 };
-
 
 export { getMembers, useGetMembers };
