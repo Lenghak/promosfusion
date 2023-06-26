708219e9a72e89f39ba5df2af781e00953f6055b
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-26 13:47:23 +0700

    [app\(main)\dashboard&members] ~ added pagetitle

--- frontend/app/(main)/dashboard/page.tsx
+++ frontend/app/(main)/dashboard/page.tsx
@@ -1,12 +1,19 @@
 import { CouponProvide } from "@/components/modules/coupon/coupon-provide";
+import { PageTitle } from "@/components/modules/page-title";
 
 import { getCurrentUser } from "@/lib/next-auth";
 
 export default async function Dashboard() {
   const session = await getCurrentUser();
   return (
-    <pre>
-      <CouponProvide campaignId="1" />
-    </pre>
+    <div>
+      <PageTitle
+        title="Dashboard"
+        description="View all your visual data associated with coupons"
+      />
+      <pre>
+        <CouponProvide campaignId="1" />
+      </pre>
+    </div>
   );
 }
--- frontend/app/(main)/members/page.tsx
+++ frontend/app/(main)/members/page.tsx
@@ -1,3 +1,20 @@
+// This memebers page fetch the data & show he list of members
+import { MemberCreateForm } from "@/components/modules/member/member-create-form";
+import { PageTitle } from "@/components/modules/page-title";
+
+// import { getQueryClient } from "@/lib/react-query";
+
 export default function Members() {
-  return <div>Members</div>;
+  // const queryClient = getQueryClient()
+  // queryClient.prefetchQuery([])
+
+  return (
+    <div className="flex h-full w-full flex-col">
+      <PageTitle
+        title="Dashboard"
+        description="View all your visual data associated with coupons"
+      />
+      <MemberCreateForm />
+    </div>
+  );
 }
