75b57e14cdee61943a19fb5d3a9f5d8784491c55
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-28 16:34:02 +0700

    [lib\utils] ~ added dateFormat function

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -1,6 +1,19 @@
-import { ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+import { MONTHS } from "@/constants/months";
+import { ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
+
+export const dateFormat = (string: string) => {
+  if (!string) return "";
+
+  const date = new Date(string);
+
+  const day = date.getDate();
+  const month: number = date.getMonth();
+  const year = date.getFullYear();
+
+  return `${day}-${MONTHS[month]}-${year}`;
+};
