e65b8c9389b33dde4ba9fc0c47c3819adad3dcd1
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-28 22:04:28 +0700

    [next.config.js] ~ allow all image host

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/dashboard',
-                permanent: true,
-            },
-        ];
-    },
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
