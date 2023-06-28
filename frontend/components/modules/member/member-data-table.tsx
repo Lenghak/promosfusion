f497aa7aef14a1e825a4fc586c96a9f64c8728e3
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-28 10:55:41 +0700

    [mod\member-data-table] ~ added table

--- frontend/components/modules/member/member-data-table.tsx
+++ frontend/components/modules/member/member-data-table.tsx
@@ -1,15 +1,32 @@
 "use client";
 
+import { DataTable } from "@/components/ui/data-table";
+
 import { useGetMembersService } from "@/services/member";
+
+import { MemberCreateForm } from "./member-create-form";
+import { MemberColumns } from "./member-table-columns";
 
 type MemberDataTableProps = {};
 
 const MemberDataTable = ({}: MemberDataTableProps) => {
   const { data: members } = useGetMembersService();
 
-  console.log(members);
-
-  return <div>MemberDataTable</div>;
+  return (
+    <div className="h-full">
+      <DataTable
+        widget={
+          <>
+            <MemberCreateForm />
+          </>
+        }
+        data={members?.data ?? []}
+        columns={MemberColumns}
+        filterBy="name"
+        tableContainerClass="h-[60vh] overflow-y-auto"
+      />
+    </div>
+  );
 };
 
 export { MemberDataTable };
