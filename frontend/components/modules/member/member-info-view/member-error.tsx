b7dcf85fcaf77d56a4a2c23e8752d60dbcba4f3a
lenghak.dev <lenghakhok.dev@gmail.com>
2023-06-30 18:36:16 +0700

    [mod\member-error] ~ fixed typo

--- frontend/components/modules/member/member-info-view/member-error.tsx
+++ frontend/components/modules/member/member-info-view/member-error.tsx
@@ -7,7 +7,7 @@ type Props = {};
 const MemberError = ({}: Props) => {
   return (
     <article className="my-8 flex h-full w-full flex-col items-center justify-center gap-2 self-center text-center">
-      <H3>Erorr</H3>
+      <H3>Error</H3>
       <p className="text-muted-foreground">
         There was a problem getting your data. Please try again later.
       </p>
