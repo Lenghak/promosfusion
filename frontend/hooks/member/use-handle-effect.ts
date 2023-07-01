4b12984aaba602be825a17c8dbf3e0086de8740d
lenghak.dev <lenghakhok.dev@gmail.com>
2023-07-01 19:06:10 +0700

    [hooks\use-handle-effect] ~ updated & delete

--- frontend/hooks/member/use-handle-effect.ts
+++ frontend/hooks/member/use-handle-effect.ts
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 import { UseFormReturn } from "react-hook-form";
 
+import { redirect } from "next/navigation";
+
+import { useDialogStore } from "@/lib/zustand";
+
 import { useToast } from "@/hooks/use-toast";
 import { AxiosError, isAxiosError } from "axios";
 
@@ -10,16 +14,12 @@ const useHandleCreatedEffect = (
   isSuccess: boolean,
   openDialog: (state: boolean, id?: string) => void,
   error: Error | AxiosError,
-  form: UseFormReturn<
-    {
-      name: string;
-      email: string;
-      password: string;
-      role: "manager" | "seller";
-    },
-    any,
-    undefined
-  >
+  form: UseFormReturn<{
+    name: string;
+    email: string;
+    password: string;
+    role: "manager" | "seller";
+  }>
 ) => {
   const { toast } = useToast();
 
@@ -42,7 +42,58 @@ const useHandleCreatedEffect = (
         title: "Member Created Successfully",
       });
     }
-  }, [isError, isSuccess, toast, openDialog, form, error]);
+  }, [isError, isSuccess, toast, openDialog, form, error, dialogID]);
 };
 
-export { useHandleCreatedEffect };
+const useHandleDeleteEffect = (isError: boolean, isSuccess: boolean) => {
+  const { toast } = useToast();
+
+  useEffect(() => {
+    if (isSuccess) {
+      toast({
+        title: "Account deleted failed",
+        description: "There was an error deleting the account.",
+      });
+      redirect("/members");
+    }
+
+    if (isError) {
+      toast({
+        title: "Account deleted Successfully",
+        description: "User that the account can no longer logged in",
+      });
+    }
+  }, [isError, isSuccess, toast]);
+};
+
+const useHandleUpdatedEffect = (
+  isError: boolean,
+  isSuccess: boolean,
+  dialogId: string
+) => {
+  const { toast } = useToast();
+  const openDialog = useDialogStore((state) => state.openDialog);
+
+  useEffect(() => {
+    if (isError)
+      toast({
+        title: "Member Update Failed",
+        description: "There was a problem updating the member",
+      });
+
+    if (isSuccess) {
+      toast({
+        title: "Member Updated Successfully",
+        description:
+          "Your changed has been saved. Your activity has been recorded.",
+      });
+      openDialog(false, dialogId);
+    }
+  }, [toast, isError, isSuccess, dialogId]);
+};
+
+export {
+  useHandleCreatedEffect,
+  useHandleDeleteEffect,
+  useHandleUpdatedEffect,
+};
