953fc07b3180ac4fd14d8b9de209d91595dd72c6
lenghak.dev <lenghakhok.dev@gmail.com>
2023-07-01 22:35:43 +0700

    [lib\axios] ~ added async to returned func

--- frontend/lib/axios/member.ts
+++ frontend/lib/axios/member.ts
@@ -24,32 +24,34 @@ const getMember = async (id: string) => {
 //* get members list function for client side
 const useGetMembers = () => {
   const authorizedAxios = useAxiosAuth();
-  return () => authorizedAxios.get<Members>("/users");
+  return async () => authorizedAxios.get<Members>("/users");
 };
 
 //* get individual user by id for client side
 const useGetMember = () => {
   const authorizedAxios = useAxiosAuth();
-  return (id: string) => authorizedAxios.get<{ data: Member }>(`/users/${id}`);
+  return async (id: string) =>
+    authorizedAxios.get<{ data: Member }>(`/users/${id}`);
 };
 
 //* create a member
 const useCreateMember = () => {
   const authorizedAxios = useAxiosAuth();
-  return (member: CreateMemberData) =>
+  return async (member: CreateMemberData) =>
     authorizedAxios.post<CreatedMemberResponse>("/users", member);
 };
 
 //* update an existing member
 const useUpdateMember = () => {
   const authorizedAxios = useAxiosAuth();
-  return (member: Member) => authorizedAxios.put(`/users/${member.id}`, member);
+  return async (member: Member) =>
+    authorizedAxios.put(`/users/${member.id}`, member);
 };
 
 //* delete an existing member
 const useDeleteMember = () => {
   const authorizedAxios = useAxiosAuth();
-  return (id: string) => authorizedAxios.delete(`/users/${id}`);
+  return async (id: string) => authorizedAxios.delete(`/users/${id}`);
 };
 
 export {
