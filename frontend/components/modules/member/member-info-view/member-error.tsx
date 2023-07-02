1c765e6f3d2782e4660cfa4704dac0364f728cfa
lenghak.dev <lenghakhok.dev@gmail.com>
2023-07-02 22:00:22 +0700

    [mod\member-error] ~ added option for 404

--- frontend/components/modules/member/member-info-view/member-error.tsx
+++ frontend/components/modules/member/member-info-view/member-error.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { H3 } from "@/components/ui/h3";
 
-type Props = {};
+type MemberErrorProps = {
+  status: string;
+};
 
-const MemberError = ({}: Props) => {
+const MemberError = ({ status }: MemberErrorProps) => {
   return (
-    <article className="my-8 flex h-full w-full flex-col items-center justify-center gap-2 self-center text-center">
-      <H3>Error</H3>
-      <p className="text-muted-foreground">
-        There was a problem getting your data. Please try again later.
-      </p>
+    <article className="my-8 flex h-full min-h-[60vh] w-full flex-col items-center justify-center gap-2 self-center text-center">
+      {status === "404" ? (
+        <Fragment>
+          <H3>404 | Error</H3>
+          <p className="text-muted-foreground">
+            No member found or has been deleted.
+          </p>
+        </Fragment>
+      ) : (
+        <Fragment>
+          <H3>Error</H3>
+          <p className="text-muted-foreground">
+            There was a problem getting your data. Please try again later.
+          </p>
+        </Fragment>
+      )}
     </article>
   );
 };
