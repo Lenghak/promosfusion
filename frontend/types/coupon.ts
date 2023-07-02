5bb255f840eeabb85118dab4f9d3a12b30d99d79
lenghak.dev <lenghakhok.dev@gmail.com>
2023-07-02 18:10:48 +0700

    [coupons\type] ~ added list coupon

--- frontend/types/coupon.ts
+++ frontend/types/coupon.ts
@@ -39,3 +39,34 @@ export type CouponRequestResponse = {
 export type CouponVerifyResponse = {
   message: string;
 };
+
+//* lists
+export interface Coupons {
+  data: Coupon[];
+  links: CouponsPageLinks;
+  meta: CouponsMeta;
+}
+
+export interface CouponsPageLinks {
+  first: string;
+  last: string;
+  prev: any;
+  next: string;
+}
+
+export interface CouponsMeta {
+  current_page: number;
+  from: number;
+  last_page: number;
+  links: CouponsMetaLink[];
+  path: string;
+  per_page: number;
+  to: number;
+  total: number;
+}
+
+export interface CouponsMetaLink {
+  url?: string;
+  label: string;
+  active: boolean;
+}
