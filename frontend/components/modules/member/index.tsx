9257145b31db101abdf01ec2239b4f2ef576623c
lenghak.dev <lenghakhok.dev@gmail.com>
2023-07-02 21:58:40 +0700

    [mod\member-delete-form] ~ added a file for delete dialog

--- frontend/components/modules/member/index.tsx
+++ frontend/components/modules/member/index.tsx
@@ -2,3 +2,4 @@ export { MemberCreateForm } from "./member-create-form";
 export { MemberDataTable } from "./member-data-table";
 export { MemberColumns } from "./member-table-columns";
 export { MemberInfoView } from "./member-info-view/member-info-view";
+export { MemberDeleteForm } from "./member-delete-form";
