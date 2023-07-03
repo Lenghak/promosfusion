46d85c7625a59347a8750da60930d9bd251ff4d8
lenghak.dev <lenghakhok.dev@gmail.com>
2023-07-03 19:51:07 +0700

    [mod\page-title] ~ added className

--- frontend/components/modules/page-title/page-title.tsx
+++ frontend/components/modules/page-title/page-title.tsx
@@ -5,15 +5,18 @@ import { Fragment, useMemo } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+import { cn } from "@/lib/utils";
+
 type PageTitleProps = {
   title: string;
   description: string;
+  className?: string;
 };
 
-const PageTitle = ({ title, description }: PageTitleProps) => {
+const PageTitle = ({ title, description, className }: PageTitleProps) => {
   const pathName: string[] = usePathname().split("/");
 
-  const breadCrumpPath: string[] = useMemo(() => {
+  const breadCrumbPath: string[] = useMemo(() => {
     const array: string[] = [];
     pathName.reduce((acc, current, index) => {
       array[index] = acc + "/" + current;
@@ -24,15 +27,20 @@ const PageTitle = ({ title, description }: PageTitleProps) => {
   }, [pathName]);
 
   return (
-    <section className="flex w-full items-center justify-between gap-1 py-4">
+    <section
+      className={cn(
+        "flex w-full items-center justify-between gap-1 py-4",
+        className
+      )}
+    >
       <div className="flex flex-col items-start justify-center">
         <span className="text-2xl font-bold">{title}</span>
         <span className="text-sm text-muted-foreground">{description}</span>
       </div>
 
-      {/* Bread Crump */}
+      {/* Bread Crumb */}
       <div className="hidden gap-2 text-center text-sm font-semibold capitalize sm:flex">
-        {breadCrumpPath.map((path, index) => (
+        {breadCrumbPath.map((path, index) => (
           <Fragment key={index}>
             /<Link href={path}>{pathName[index + 1]}</Link>
           </Fragment>
