1cb06c192603e3b99ecbdff5671e06b1eccf6c52
lenghak.dev <lenghakhok.dev@gmail.com>
2023-07-06 14:19:26 +0700

    [mod\shop-assign-member] ~ added multi-select

--- frontend/components/modules/shop/shop-info-view/shop-info-members/shop-assign-member-form.tsx
+++ frontend/components/modules/shop/shop-info-view/shop-info-members/shop-assign-member-form.tsx
@@ -2,25 +2,31 @@
 
 import { useForm } from "react-hook-form";
 
+import { AvatarCard } from "@/components/modules/avatar-card";
 import { DialogWithAlert } from "@/components/modules/dialog-with-alert";
-import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "@/components/ui/button";
+import { DialogTrigger } from "@/components/ui/dialog";
 import {
   Form,
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { MultiSelect } from "@/components/ui/select/multi-select";
+
+import { useDialogStore } from "@/lib/zustand";
 
 import { useGetMembersService } from "@/services/member";
 
 import { zodResolver } from "@hookform/resolvers/zod";
+import { UserPlus } from "lucide-react";
 import z from "zod";
 
 type ShopAssignFormProps = {
   dialogTrigger?: React.ReactNode;
   dialogID: string;
+  shopId: string;
 };
 
 const ShopAssignSchema = z.object({
@@ -29,15 +35,25 @@ const ShopAssignSchema = z.object({
   }),
 });
 
-const ShopAssignForm = ({ dialogTrigger, dialogID }: ShopAssignFormProps) => {
-  // const openAlert = useDialogStore((state) => state.openAlert);
+const ShopAssignForm = ({
+  dialogTrigger,
+  dialogID,
+  shopId,
+}: ShopAssignFormProps) => {
+  const openAlert = useDialogStore((state) => state.openAlert);
 
   const { data: members } = useGetMembersService();
 
   const form = useForm<z.infer<typeof ShopAssignSchema>>({
     resolver: zodResolver(ShopAssignSchema),
     defaultValues: {
-      memberIds: [],
+      memberIds:
+        members?.data.reduce((acc: string[], current) => {
+          if (current.shopIds.includes(parseInt(shopId)))
+            acc.push(`${current.id}`);
+
+          return acc;
+        }, []) ?? [],
     },
     shouldUnregister: true,
   });
@@ -45,7 +61,19 @@ const ShopAssignForm = ({ dialogTrigger, dialogID }: ShopAssignFormProps) => {
   return (
     <DialogWithAlert
       id={dialogID}
-      dialogTrigger={dialogTrigger}
+      dialogTrigger={
+        dialogTrigger ?? (
+          <DialogTrigger
+            className="p-2"
+            asChild
+          >
+            <Button className="w-fit gap-4 px-3 lg:px-4">
+              <UserPlus size={18} />
+              <span className="hidden lg:inline-block">Add Member</span>
+            </Button>
+          </DialogTrigger>
+        )
+      }
       dialogTitle={"Assign a Member to Shop"}
       dialogDescription={"Select a shop from the list to assign"}
       alertTitle={"Are you absolutely sure?"}
@@ -53,55 +81,55 @@ const ShopAssignForm = ({ dialogTrigger, dialogID }: ShopAssignFormProps) => {
         "You are about to close this dialog. All your changes will be unsaved."
       }
     >
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit((values) => console.log(values))}
-          className="space-y-8"
-        >
-          <FormField
-            control={form.control}
-            name="memberIds"
-            render={() => (
-              <FormItem>
-                <div className="mb-4">
-                  <FormLabel className="text-base">Sidebar</FormLabel>
-                </div>
-                {members?.data.map((member) => (
-                  <FormField
-                    key={member.id}
-                    control={form.control}
-                    name="memberIds"
-                    render={({ field }) => {
-                      return (
-                        <FormItem
-                          key={member.id}
-                          className="flex flex-row items-start space-x-3 space-y-0"
-                        >
-                          <FormControl>
-                            <Checkbox
-                              checked={field.value?.includes(`${member.id}`)}
-                              onCheckedChange={(checked) => {
-                                return checked
-                                  ? field.onChange([...field.value, member.id])
-                                  : field.onChange(
-                                      field.value?.filter(
-                                        (value) => value !== `${member.id}`
-                                      )
-                                    );
-                              }}
-                            />
-                          </FormControl>
-                        </FormItem>
-                      );
-                    }}
-                  />
-                ))}
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </form>
-      </Form>
+      {members?.data ? (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit((values) => console.log(values))}
+            className="space-y-4"
+          >
+            <FormField
+              control={form.control}
+              name={"memberIds"}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <MultiSelect
+                      onChange={(values) => {
+                        field.onChange(values.map(({ value }) => value));
+                      }}
+                      data={members.data.map((member) => ({
+                        label: (
+                          <AvatarCard
+                            info={member.email}
+                            name={member.name}
+                            image={member.avatar}
+                          />
+                        ),
+                        value: `${member.id}`,
+                        badge: member.name,
+                      }))}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex justify-end gap-4">
+              <Button
+                type="button"
+                variant={"outline"}
+                onClick={() => openAlert(true, dialogID)}
+              >
+                Cancel
+              </Button>
+              <Button type="submit">Update</Button>
+            </div>
+          </form>
+        </Form>
+      ) : (
+        <div>There are no member found.</div>
+      )}
     </DialogWithAlert>
   );
 };
