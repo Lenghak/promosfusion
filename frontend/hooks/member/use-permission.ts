00dd3d14eda6efbabb69f587ae0fdb03d8cfde1d
lenghak.dev <lenghakhok.dev@gmail.com>
2023-07-07 13:01:35 +0700

    [hooks\member] ~ update permission

--- frontend/hooks/member/use-permission.ts
+++ frontend/hooks/member/use-permission.ts
@@ -1,26 +1,30 @@
 import { useSession } from "next-auth/react";
+import z from "zod";
 
 import { Member } from "@/types/member";
+
+const ActionSchema = z.enum(["c", "r", "u", "d"]);
 
 const usePermission = () => {
   const { data: session } = useSession();
 
-  return (accessing_user: Member) => {
+  return (accessingUser: Member, action: z.infer<typeof ActionSchema>) => {
     //* if the current is not accessing the self && not just reading :
-    if (accessing_user.uuid !== session?.user.uuid) {
+    if (accessingUser.uuid !== session?.user.uuid) {
       switch (session?.user.role) {
         case "root": {
           //* return true if reading other root user info
-          return accessing_user.role !== "root";
+          return true;
         }
         case "manager": {
-          return accessing_user.role === "seller";
+          return accessingUser.role === "seller";
         }
         default: {
           return false;
         }
       }
-    } else return true;
+    }
+    return action !== "d";
     //* if the current is accessing self info -> return true
   };
 };
